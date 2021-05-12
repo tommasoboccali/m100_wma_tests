bf6b7542342e10484f2f09475ef5bc167427bd85
tommaso boccali <tboccali@login02.m100.cineca.it>
2021-05-12 09:58:53 +0200

    adding slurm debug and tsocks

--- slurm_submission/slurm_script.sh
+++ slurm_submission/slurm_script.sh
@@ -12,6 +12,28 @@ export SING_SCRIPT=$GLIDEINSCRIPTS/sing_script.sh
 
 module load singularity
 
+
+if [ "$use_tsocks" == 1 ]
+then
+	echo ===== SETTING FOR SINGULARITY WITH  $tsocks_ssh_cmd
+	$tsocks_ssh_cmd &
+	echo ===== SLEEPING FOR 20 SEC
+	sleep 20
+	cd $WORK/tsocks-1.8beta5+ds1/
+	export LD_PRELOAD=`pwd`/libtsocks.so
+	cd -
+
+fi
+
+
+if [ "$use_tsocks" == 1 ]
+then
+	LD_PRELOAD=$WORK/tsocks-1.8beta5+ds1/libtsocks.so singularity run -B /cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security   -B /m100_work -B /cvmfs -B /m100_work/INF21_lhc_0/CMS/SITECONF:/marconi_work/Pra18_4658/cms/SITECONF $IMAGE $SING_SCRIPT
+else
+	 singularity run -B /cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security   -B /m100_work -B /cvmfs -B /m100_work/INF21_lhc_0/CMS/SITECONF:/marconi_work/Pra18_4658/cms/SITECONF $IMAGE $SING_SCRIPT
+fi
+
+
 singularity run -B /cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security   -B /m100_work -B /cvmfs -B /m100_work/INF21_lhc_0/CMS/SITECONF:/marconi_work/Pra18_4658/cms/SITECONF $IMAGE $SING_SCRIPT
 
 
