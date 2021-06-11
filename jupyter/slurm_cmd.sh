5fda59883ae0f5b3864f2e45e12ae05f07e93bfe
Tommaso Boccali <Tommaso.Boccali@cern.ch>
2021-06-11 20:54:31 +0200

    fixes

--- jupyter/slurm_cmd.sh
+++ jupyter/slurm_cmd.sh
@@ -1,10 +1,15 @@
 # assume to be filled by the jupyterhub
 IP=the_ip_to_call_back
 TOKEN=the_token_tobe_used
+USERIAM=user_name_from_iam
 
 #decide
 use_tsocks=1
 tsocks_ssh_cmd="ssh -N -D 1087 -o TCPKeepAlive=yes -o ServerAliveInterval=60 boccali@lxplus.cern.ch"
+
+#various
+jup_exec=jypyterhub-standalone
+root_dir=$WORK/jupRoot
 
 if [ "$use_tsocks" == 1 ]
 then
@@ -17,8 +22,19 @@ then
         cd -
 fi
 
+
 module load anaconda
 conda activate testJH
+
+mkdir -p $root_dir
+cd $root_dir
+mkdir -p $USERIAM
+cd $USERIAM
+
 export JUPYTERHUB_API_TOKEN=${TOKEN}
-export JUPYTERHUB_SERVIE_URL=${IP}
+export JUPYTERHUB_SERVICE_URL=${IP}
+
+$jup_exe
+
+exit $?
 
