1cab2cc0f94856e40eb63346b9ee5ca50c931fa8
Tommaso Boccali <tommaso.boccali@gmail.com>
2021-06-15 13:04:10 +0200

    add first version

---
+++ jupyter/slurm_script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# i cannot set the tsocks from here, I skip
+
+echo == SLURM SCRIPT starting at `date`
+
+export WORK_AREA=$WORK/jh_work_area
+mkdir -p $WORK_AREA
+
+module load anaconda
+conda avtivate 
+
+echo ######### STARTING JOB
+
+if [ "$use_tsocks" == 1 ]
+then
+	echo ===== SETTING FOR SINGULARITY WITH  $tsocks_ssh_cmd
+	$tsocks_ssh_cmd &
+	echo ===== SLEEPING FOR 20 SEC
+	sleep 20
+	cd $WORK/tsocks-1.8beta5+ds1/
+	export LD_PRELOAD=`pwd`/libtsocks.so
+	cd -
+
+fi
+
+export JUPYTERHUB_API_TOKEN={thetoken}
+export JUPYTERHUB_API_URL={theurl}
+export USERNAME={username}
+
+mkdir -p $USERNAME
+cd $USERNAME
+
+jupyterhub-singleuser 
+
+
+echo == SLURM SCRIPT finishing at `date` with code $?
+
+exit 0
